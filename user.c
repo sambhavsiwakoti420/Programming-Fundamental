e5c42cd144c6d07ed624da42b494113a1002db39
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-14 12:53:16 +0545

    Add files via upload

---
+++ user.c
@@ -0,0 +1,22 @@
+/*
+Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+ROll No.:
+Lab Sheet no.:
+Program:Write a C program to print your name,ask from user
+Date:14 November 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+    int main(){
+
+char A[5];
+
+printf("what is your name?\n");
+scanf("%s",&A);
+printf("Name of user is:%s",A);
+getch();
+return 0;
+    }
