c69529c8d481c4b482baa42220b77cb0c39e177d
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-14 10:40:32 +0545

    Add files via upload

---
+++ sambhav.c
@@ -0,0 +1,23 @@
+/*
+Student name:Sambhav siwakoti
+Subject:Programming Fundamental
+Roll No.:
+Lab Sheet No.:
+Programme:Write a C programme to print your name
+Date:14 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+
+      int main(){
+
+printf("Sambhav Siwakoti");
+
+      getch();
+
+
+
+
+
+return 0;
+}
