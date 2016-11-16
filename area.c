1ea27ef34b306079e1b3faa0ad1cf360d4535e60
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-16 12:56:12 +0545

    Add files via upload

---
+++ area.c
@@ -0,0 +1,31 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:
+Lab sheet No.:2
+Program:Write a C program to print the division of two number asked from user.
+Date:16 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  float b,h,area;
+
+  printf("enter the base of triangle \n");
+  scanf("%f",&b);
+  printf("enter the height of triangle \n");
+  scanf("%f",&h);
+  area=0.5*b*h;
+  printf("the area of triangle is:%f",area);
+
+
+
+
+
+
+
+
+
+    getch();
+    return 0;
+}
