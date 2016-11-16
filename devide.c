035d6d110c6c4083c36b0057e02d42f4c47bc7a6
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-16 12:24:18 +0545

    Add files via upload

---
+++ devide.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:
+Lab sheet No.:2
+Program:Write a C program to print the division of two number asked from user.
+Date:16 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float a,b,divide;
+printf("Enter the first number:  \n");
+scanf("%f",&a);
+printf("Enter the second number:  \n");
+scanf("%f",&b);
+divide=a/b;
+printf("the division of two number is:%f",divide);
+
+
+getch ();
+return 0;
+}
