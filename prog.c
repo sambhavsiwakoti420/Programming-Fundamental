9603794a2c1ef014c7b87916af390bd36592a50e
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-16 10:26:01 +0545

    Add files via upload

---
+++ prog.c
@@ -0,0 +1,26 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:
+Labsheet No.:2
+Program:Write a C program to prime sum of two numbers,asked from user.
+Date:16 Nov. 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+ int main(){
+ int a,b,sum;
+  printf("Enter the  first number\n");
+ scanf("%d",&a);
+ printf("Enter the second number\n");
+ scanf("%d",&b);
+ sum=a+b;
+ printf("Sum of numbers is:%d",sum);
+
+
+
+
+getch();
+ return 0;
+ }
