e1aa97398b75771e923a71086fe38b5db72da9fe
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-16 10:49:59 +0545

    Add files via upload

---
+++ product.c
@@ -0,0 +1,25 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll no.:
+Lab sheet No.:2
+Program:Write a C program to print the product of two numbers asked from user.
+Date:16 Nov. 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a,b,product;
+printf("Enter the first number  \n");
+scanf("%d",&a);
+printf("Enter the second number  \n");
+scanf("%d",&b);
+product=a*b;
+printf("product of two number is:%d",product);
+
+
+
+getch();
+return 0;
+
+}
