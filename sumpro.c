c7350b563765cbd2c5b7917990a4f760b671e04b
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-16 13:14:05 +0545

    Add files via upload

---
+++ sumpro.c
@@ -0,0 +1,33 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:
+Lab sheet No.:2
+Program:Write a C program to print the division of two number asked from user.
+Date:16 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  int a,b,c,sum,product;
+  printf("Enter the first number  \n");
+  scanf("%d",&a);
+  printf("Enter the second number  \n");
+  scanf("%d",&b);
+  printf("Enter the third number  \n");
+  scanf("%d",&c);
+  sum=a+b+c;
+  product=a*b*c;
+  printf("sum of three numbers is:%d  \n",sum);
+  printf("product of three numbers is:%d  \n",product);
+
+
+
+
+
+
+
+
+    getch();
+    return 0;
+}
