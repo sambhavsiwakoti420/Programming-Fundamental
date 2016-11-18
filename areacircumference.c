13bb3ccdc386d244d16dfce11759f931785722ec
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-18 12:29:43 +0545

    Add files via upload

---
+++ areacircumference.c
@@ -0,0 +1,30 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:
+Lab Sheet NO.:3
+Program:Write a C program to calculate the area and circumference of circle having radius,radius should be taken
+Date:18 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+float r,area,circumference;
+printf("Enter the value of r \n");
+scanf("%f",&r);
+area=3.14*r*r;
+printf("The area of circle is:%f \n",area);
+circumference=2*3.14*r;
+printf("The circumference of circle is:%f \n",circumference);
+
+
+
+
+
+
+
+
+
+getch();
+return 0;
+}
