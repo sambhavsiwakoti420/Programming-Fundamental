69e2cef0960f923d0911dcc496ce5359a3cca46b
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-23 10:23:22 +0545

    Add files via upload

---
+++ Temperature.c
@@ -0,0 +1,24 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet No.:05
+Program:Write a C program to read temperature in fahrenit and convert it into centigrate.
+Date:23 November 2016
+*/
+#include<stdio.h>
+int main(){
+	float f,centigrade;
+	printf("Enter temperature in fahrenit \n");
+	scanf("%f",&f);
+	
+centigrade=(0.555)*(f-32);
+printf("fahrenhit into centigrade is:%f\n",centigrade);
+
+	
+	
+	
+	
+	
+	return 0;
+	}
