993b01814633af095baf7df74fc81fa4d44b3b42
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-23 13:02:24 +0545

    Add files via upload

---
+++ Quadraticrootnew.c
@@ -0,0 +1,28 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet No.:05
+Program:Write a C program to find roots of quadratic equation and value are assigned from user.
+Date:23 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float x1=0,x2=0,a,b,c,dis=0;
+    printf("Enter the value of a \n");
+    scanf("%f",&a);
+    printf("Enter the value of b \n");
+    scanf("%f",&b);
+    printf("Enter the value of c \n");
+    scanf("%f",&c);
+   dis=(b*b)-(4*a*c);
+   x1=(-b+pow(dis,0.5))/(2*a);
+   x2=(-b-pow(dis,0.5))/(2*a);
+   printf("The first root is:%f \n",x1);
+   printf("The second root is:%f \n",x2);
+
+
+getch();
+return 0;
+}
