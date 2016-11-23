abc2870ba5e3ddf11754d37760b1e3b3803fb861
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-23 10:09:55 +0545

    Add files via upload

---
+++ sumofthreedigitsnumber.c
@@ -0,0 +1,27 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet NO.:05
+Program:Write a C program to print sum of three numbers
+Date:23 November 2016
+*/
+#include<stdio.h>
+
+int main (){
+	int a,b,c,sum;
+	printf("Enter the first number \n");
+	scanf("%d",&a);
+	printf("Enter the second number \n");
+	scanf("%d",&b);
+	printf("Enter the third number \n");
+	scanf("%d",&c);
+	sum=a+b+c;
+	printf("The sum of three numbers is:%d \n",sum);
+	
+	
+	
+	
+	return 0;
+	
+}
