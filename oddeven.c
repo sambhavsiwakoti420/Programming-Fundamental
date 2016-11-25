97e12558f9b0c05615b92541c91f1a9477fa7b17
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-25 10:49:53 +0545

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,29 @@
+/*
+Student Name:Sambhav Siwaakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet No.:06
+Program:Write a C program to check whether the number is odd or even.
+Date:24 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+    int a;
+    printf("Enter the number:\n");
+    scanf("%d",&a);
+    if (a>0){
+        printf("The number is odd:\n");
+
+    }
+    else
+    {
+        printf("The number is even:\n");
+
+    }
+ getch();
+ return 0;
+
+}
