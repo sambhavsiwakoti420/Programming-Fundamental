94f4027a19682a12ad4ec79d7868703544720b6d
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-25 13:04:24 +0545

    Add files via upload

---
+++ OR.c
@@ -0,0 +1,30 @@
+/*
+Student Name:Sambhav Siwaakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet No.:06
+Program:Write a C program to check OR.
+Date:24 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+   {
+
+int a,b;
+printf("Enter the first number \n");
+scanf("%d",&a);
+printf("Enter the second number \n");
+scanf("%d",&b);
+a||b;
+printf("|| is:%d",a||b);
+
+getch();
+return 0;
+   }
+
+
+
+
+
+
