b535ca12d5e4cdd924a7537c3f98598dd4cb7b30
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-25 10:49:31 +0545

    Add files via upload

---
+++ positivenegative.c
@@ -0,0 +1,45 @@
+/*
+Student Name:Sambhav Siwaakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet No.:06
+Program:Write a C program to check whether the number is positive or negative.
+Date:24 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+  {
+      int a;
+printf("Enter the number:\n");
+scanf("%d",&a);
+if (a>0){
+
+printf("The number is positive:%d \n");
+}
+else
+{
+
+
+    printf("The number is negative:\n");
+}
+getch();
+return 0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
