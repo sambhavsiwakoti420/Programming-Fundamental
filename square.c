b5ba14b17dea16b1dba0b7641134a976906f7a32
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-28 12:39:48 +0545

    Add files via upload

---
+++ square.c
@@ -0,0 +1,29 @@
+
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming fundamental
+ROll No.:14
+Lab Sheet No.:07
+Program:WAP to calculate square of umber ask from user.
+Date:27 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+
+float a,square,squareroot;
+printf("Enter the number  \n");
+scanf("%f",&a);
+square=pow(a,2);
+squareroot=pow(a,0.5);
+printf("Square of number is:%f \n",square);
+printf("Square root is:%f  \n",squareroot);
+
+
+
+
+
+getch();
+return 0;
+}
