b1271f7fca19e0419fcdc0eb31efd51397f489e6
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-11-29 10:02:25 +0545

    Update student marks.c

--- student marks.c
+++ student marks.c
@@ -1,41 +1,44 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:14
+Lab Sheet No.:07
+Program:Write a C program to print students's marks.
+Date:28 November 2016
+*/
 #include<stdio.h>
 #include<conio.h>
 int main()
 
 {
-   float ca,ds,is,al,om,pf, s,per;
+   float CA,DS,IS,AL,OM,PF,Sum,percentage;
 
 
 
 printf("Enter the marks of Computer architure\n");
-scanf("%f",&ca);
+scanf("%f",&CA);
 printf("Enter the marks of Discrete structure\n");
-scanf("%f",&ds);
+scanf("%f",&DS);
 printf("Enter the marks of interactive skills\n");
-scanf("%f",&is);
+scanf("%f",&IS);
 printf("Enter the marks of Algebra\n");
-scanf("%f",&al);
+scanf("%f",&AL);
 
 printf("Enter the marks of organization management\n");
-scanf("%f",&om);
+scanf("%f",&OM);
 printf("Enter the marks of Programming fundamental\n");
-scanf("%f",&pf);
-
-s=ca+ds+is+al+om+pf;
+scanf("%f",PF);
+   Sum=CA+DS+IS+AL+OM+PF;
 
 printf("Total Marks %f\n",s);
 
-per=(s/600)*100;
+percentage=(Sum/600)*100;
 
 
-printf("Your percentage is: %f ", per);
+printf("Your percentage is: %f ", percentage);
 
 getch();
-return(0);
-
-
-
-
+return 0;
 
 
 }
