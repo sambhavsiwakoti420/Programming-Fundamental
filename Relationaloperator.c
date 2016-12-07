fbf2b47cae462a5b37210917a348105210c8aa21
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-12-07 10:50:46 +0545

    Add files via upload

---
+++ Relationaloperator.c
@@ -0,0 +1,43 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:31
+Lab Sheet No.:10
+Program:WAP to print relational operator.
+Date:7 November 2016
+*/
+#include<stdio.h>
+int main (){
+int a,b;
+a=65,b=60;
+if(a>b)
+{
+    printf("%d is greater than %d \n",a,b);
+}
+if(a<b)
+{
+    printf("%d is is less than %d \n",a,b);
+}
+if(a>=b)
+
+{
+    printf("%d is greater or equal to %d \n",a,b);
+}
+
+if(a<=b)
+{
+    printf("%d is less or equal to %d \n",a,b);
+}
+
+if(a==b)
+{
+    printf("%d is equal to %d \n",a,b);
+}
+
+if(a!=b)
+
+{
+    printf("%d is not equal to %d \n",a,b);
+}
+
+}
