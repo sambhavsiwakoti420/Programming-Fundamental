e9fed07a1eecca39c617584c6b9311db1cc40ad5
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2016-12-07 11:47:20 +0545

    Add files via upload

---
+++ Ternieryoperator.c
@@ -0,0 +1,23 @@
+/*
+Student Name:Sambhav Siwakoti
+Subject:Programming Fundamental
+Roll No.:31
+Lab Sheet No.:10
+Program:WAP to demonstrarte the use of ternary operator.
+Date:7 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int n;
+printf("Enter the number \n");
+scanf("%d",&n);
+(n%2==0)?printf("The number is even"):printf("The number is odd");
+
+
+
+getch();
+return 0;
+
+}
+
