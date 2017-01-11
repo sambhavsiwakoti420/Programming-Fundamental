17ed7ae878f069ef30407073ea4a56d491d4d2e0
sambhavsiwakoti420 <sambhavsiwakoti420@gmail.com>
2017-01-11 12:23:40 +0545

    Add files via upload

--- firstandlastdigitnumber.c
+++ firstandlastdigitnumber.c
@@ -8,12 +8,13 @@ Date:11 January 2017
 */
 #include<stdio.h>
 int main(){
-	int i,n,last;
+	int i,n=0,last;
 printf("Enter the number \n");
 scanf("%d",&n);
 last=n%10;
 printf("Last digit is:%d\n",last);
-for(i=1;i<4;i++)
+i=n;
+for(i=1;i<n;i++)
 {
 	n=n/10;
 
